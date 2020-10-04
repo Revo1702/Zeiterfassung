2d7a9373c5ab813f3b39cd56427b77f4f519b4d3
Revo1702 <patyr2000@gmail.com>
2020-10-05 00:45:52 +0200

    Add files via upload

---
+++ Zeiterfassung-1.0.1/Zeiterfassung/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Zeiterfassung
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 55);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 91);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Einchecken";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 55);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 91);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Auschecken";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Noch keine Arbeitszeit aufgenommen";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Gearbeitete Zeit:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(437, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Datum und Zeit:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(552, 337);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "-";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(134, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Verbleibende Arbeitszeit:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(134, 258);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "7 Stunde/n 20 Minute/n";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(440, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Reset Time";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Zeiterfassungshilfe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Zeiterfassung-1.0.1/Zeiterfassung/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Zeiterfassung
+{
+    public partial class Form1 : Form
+    {
+        DateTime startZeit;
+        DateTime endZeit;
+        int gesamtZeit;
+        int savedTime;
+        int origTime = 26400; //26400 sec for 7h and 20min
+        int newTime = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            startZeit = DateTime.Now;
+            Console.WriteLine(startZeit);
+            string savedTimeString = ReadTxt();
+            if (savedTimeString != "0")
+            {
+                savedTime = Convert.ToInt32(savedTimeString);
+                if (savedTime != gesamtZeit)
+                {
+                    gesamtZeit += savedTime;
+                    origTime -= gesamtZeit;
+                    WriteText(Convert.ToString(gesamtZeit));
+                }
+
+            }
+            timer2.Start();
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            endZeit = DateTime.Now;
+            int dauer = (int)(endZeit - startZeit).TotalSeconds + gesamtZeit;
+            gesamtZeit = dauer;
+            WriteText(Convert.ToString(gesamtZeit));
+            string savedTimeString = ReadTxt();
+            savedTime = Convert.ToInt32(savedTimeString);
+            Console.WriteLine(gesamtZeit);
+
+
+            if (gesamtZeit > 3600)
+            {
+                int seconds = (int)gesamtZeit;
+                int minutes = seconds / 60;
+                label1.Text = String.Format("{0} Stunde/n {1} Minute/n {2} Sekunde/n", minutes / 60, minutes % 60, seconds % 60);
+            }
+            else if (gesamtZeit > 60)
+            {
+                label1.Text = String.Format("{0} Minute/n {1} Sekunde/n", gesamtZeit / 60, gesamtZeit % 60);
+            }
+            else
+            {
+                label1.Text = String.Format("{0} Sekunde/n", gesamtZeit);
+            }
+            timer2.Stop();
+            timer3.Stop();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label4.Text = DateTime.Now.ToString();
+            timer2.Stop();
+            timer3.Stop();
+            if (origTime > 3600)
+            {
+                int seconds2 = (int)origTime;
+                int minutes2 = seconds2 / 60;
+                label6.Text = String.Format("{0} Stunde/n {1} Minute/n {2} Sekunde/n", minutes2 / 60, minutes2 % 60, seconds2 % 60);
+            }
+            else
+            {
+                label6.Text = origTime / 60 + " Minuten";
+            }
+            if (origTime <= 0)
+            {
+                timer3.Start();
+                int seconds = (int)newTime;
+                int minutes = seconds / 60;
+                int hours = minutes / 60;
+                label5.Text = "Überstunden: ";
+                label6.Text = String.Format("{0} Stunde/n {1} Minute/n {2} Sekunden", hours, minutes, seconds);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label4.Text = DateTime.Now.ToString();
+            timer1.Start();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            origTime--;
+            if (origTime > 3600)
+            {
+                int seconds2 = (int)origTime;
+                int minutes2 = seconds2 / 60;
+                label6.Text = String.Format("{0} Stunde/n {1} Minute/n {2} Sekunde/n", minutes2 / 60, minutes2 % 60, seconds2 % 60);
+                //Testcode um die Sekunden zu sehen
+                //label6.Text = Convert.ToString(origTime);
+            }
+            else
+            {
+                label6.Text = origTime / 60 + " Minuten";
+            }
+            if (origTime <= 0)
+            {
+                timer2.Stop();
+                timer3.Start();
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            newTime++;
+            if (origTime <= 0)
+            {
+                timer3.Start();
+                int seconds = (int)newTime;
+                int minutes = seconds / 60;
+                int hours = minutes / 60;
+                label5.Text = "Überstunden: ";
+                label6.Text = String.Format("{0} Stunde/n {1} Minute/n {2} Sekunden", hours, minutes % 60, seconds % 60);
+            }
+            timer3.Start();
+        }
+        static void WriteText(string input)
+        {
+            Console.WriteLine("Writing...");
+            StreamWriter sw = new StreamWriter("../../../log.txt");
+            sw.WriteLine(input);
+            sw.Close();
+        }
+        static string ReadTxt()
+        {
+            Console.WriteLine("Reading..");
+            StreamReader sr = new StreamReader("../../../log.txt");
+            string line = sr.ReadLine();
+            sr.Close();
+            return line;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WriteText("0");
+            gesamtZeit = 0;
+            origTime = 26400;
+            string reset = ReadTxt();
+            Console.WriteLine(reset);
+        }
+    }
+}
